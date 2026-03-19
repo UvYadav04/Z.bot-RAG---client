0afbd26bf071b7b6d6ab06a61d3b676c93106f5b
Dinesh Yadav <dineshnirban01@gmail.com>
2026-03-19 21:39:14 +0530

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,5 +16,10 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 🔥 IMPORTANT
+    },
+  },
 })
